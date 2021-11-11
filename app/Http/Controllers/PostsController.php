9827e52b703454b9f1058e952087f7e5a96b815a
Yunesh Shrestha <yuneshshrestha24@gmail.com>
2021-11-11 15:08:26 +0545

    blog index created

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -13,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        return view('blog.index');
+        $posts= Post::orderBy('updated_at','DESC')->get();
+        return view('blog.index')->with('posts', $posts);
     }
 
     /**
--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,23 +2,25 @@
 @section('content')
     <div class="container blog pt-4">
         <div class="text-center blog_posts">Blog Posts</div>
-        <div class="row">
-            <div class="col-md-6 col-12 my-auto">
-                <img src="https://images.pexels.com/photos/459653/pexels-photo-459653.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="img-fluid">
-            </div>
-            <div class="col-md-6 col-12 my-auto">
-                <div class="blog_title">Learn something new</div>
-                <div class="blog_by">By <span class="blog_by_user_name font-italic font-weight-bold">Yunesh Shrestha</span>, 1day ago</div>
-                <div class="blog_description">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas sapiente provident excepturi aut inventore, similique a temporibus tempore veniam natus ex autem deleniti et distinctio doloremque, tempora, quae iusto id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, eligendi.
+        @foreach ($posts as $post)
+            <div class="row pt-4">
+                <div class="col-md-6 col-12 my-auto">
+                    <img src="https://images.pexels.com/photos/459653/pexels-photo-459653.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" class="img-fluid">
                 </div>
-                <div class="blog_btn">
-                    <button class="btn btn-primary rounded-pill text-uppercase">
+                <div class="col-md-6 col-12 my-auto">
+                    <div class="blog_title">{{ $post->title }}</div>
+                    <div class="blog_by">By <span class="blog_by_user_name font-italic font-weight-bold">{{ $post->user->name }}</span>, Created On: {{ date('jS M Y', strtotime($post->updatedAt)) }} </div>
+                    <div class="blog_description">
+                        {{ $post->description }}
+                    </div>
+                    <div class="blog_btn">
+                        <button class="btn btn-primary rounded-pill text-uppercase">
 
-                        Keep Reading
-                    </button>
+                            Keep Reading
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        @endforeach
     </div>
 @endsection
